767c046854d6cb5fda2b5084a058c05bee16bd46
Sophie Branigan <sophie@aloud.ie>
2013-07-15 11:11:57 +0100

    Right, created awesomebook rails application, ACTUALLY commited it this time, and haven't added any status updates yet.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesomebook::Application.config.secret_token = '553a3d78eee5e625f5b0f63e533c198a4ebb6f9e43ca7f21bdc0aafca8a6a757188cfe07f88b6a2a145a957d12561116c16f33e3d9aadc318d7bb573fdf7206b'
